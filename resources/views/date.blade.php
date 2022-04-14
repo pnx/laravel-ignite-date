f90eebfe96dbf8a14e4cc7ca20e3adcb24237572
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2022-04-14 17:50:05 +0200

    resources/views/date.blade.php: wrap input in a div with wire:ignore attribute.

--- resources/views/date.blade.php
+++ resources/views/date.blade.php
@@ -1,9 +1,11 @@
-<x-ignite-input
-    x-data='{}'
-    x-init="flatpickr($refs.this, {{ $jsonOptions() }})"
-    x-ref='this'
-    :id="$id"
-    :name="$name"
-    type="text"
-    {{ $attributes }}
-/>
+<div wire:ignore>
+    <x-ignite-input
+        x-data='{}'
+        x-init="flatpickr($refs.this, {{ $jsonOptions() }})"
+        x-ref='this'
+        :id="$id"
+        :name="$name"
+        type="text"
+        {{ $attributes }}
+    />
+</div>
