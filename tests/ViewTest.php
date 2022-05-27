a9dd6b91a05a673692a3513630df7de42cf53898
Henrik Hautakoski <henrik.hautakoski@gmail.com>
2022-05-27 11:13:33 +0200

    tests/ViewTest.php: Adding date-range and date-time tests

--- tests/ViewTest.php
+++ tests/ViewTest.php
@@ -30,4 +30,38 @@ class ViewTest extends TestCase
             '>'
         ], false);
     }
+
+    public function test_can_render_date_range_component()
+    {
+        $this->withViewErrors([]);
+
+        $view = $this->blade('<x-ignite-date-range id="date-range-id" name="xdatex" />');
+
+        // Assert that the input element contains some valid data.
+        $view->assertSeeInOrder([
+            '<input',
+            'id="date-range-id"',
+            'name="xdatex"',
+            'x-data="{}"',
+            'x-init="flatpickr',
+            '>'
+        ], false);
+    }
+
+    public function test_can_render_date_time_component()
+    {
+        $this->withViewErrors([]);
+
+        $view = $this->blade('<x-ignite-date-time id="date-time-id" name="xdatex" />');
+
+        // Assert that the input element contains some valid data.
+        $view->assertSeeInOrder([
+            '<input',
+            'id="date-time-id"',
+            'name="xdatex"',
+            'x-data="{}"',
+            'x-init="flatpickr',
+            '>'
+        ], false);
+    }
 }
